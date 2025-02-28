51cc97c0b5c9804f1d7d29183a903182ff00fec8
Vera Spiridonova <ver-spiridonova@yandex.ru>
2025-02-28 14:04:44 +0300

    Added context in views.py

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,8 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +11,16 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phone_objects = Phone.objects.all()
+    context = {'phones': phone_objects}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    try:
+        phone = Phone.objects.get(slug=slug)
+        context = {'phone': phone}
+        return render(request, template, context)
+    except ObjectDoesNotExist:
+        return HttpResponse("Телефон не найден!")
