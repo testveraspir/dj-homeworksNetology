89f884e82229edc09c7ff2b476dd7586cda3c1db
Vera Spiridonova <ver-spiridonova@yandex.ru>
2025-03-01 23:00:32 +0300

    Added converter

--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, register_converter
 
+from books.converters import DateConverter
 from books.views import books_view
+
+register_converter(DateConverter, 'date')
 
 urlpatterns = [
     path('', books_view),
