eeba40b26225959dcac74783cb8fef6fbf458670
Vera Spiridonova <ver-spiridonova@yandex.ru>
2025-03-03 20:59:03 +0300

    Updated views.py

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -9,20 +9,32 @@ def index(request):
     return redirect('catalog')
 
 
-def show_catalog(request):
-    template = 'catalog.html'
-    phone_objects = Phone.objects.all()
-    sort = request.GET.get('sort', 'name')
-    if sort == 'name':
-        phone_objects = phone_objects.order_by("name")
-    elif sort == 'min_price':
-        phone_objects = phone_objects.order_by("price")
-    elif sort == 'max_price':
-        phone_objects = phone_objects.order_by("-price")
+def get_sort_param(sort_param):
+    if sort_param == 'name':
+        return 'name'
+    elif sort_param == 'min_price':
+        return 'price'
+    elif sort_param == 'max_price':
+        return '-price'
     else:
+        raise ValueError(f'Некорректный параметр сортировки: {sort_param}')
+
+
+def show_catalog(request):
+    try:
+        template = 'catalog.html'
+        phone_objects = Phone.objects.all()
+        sort = request.GET.get('sort', 'name')
+        sort_name = get_sort_param(sort)
+        phone_objects = phone_objects.order_by(sort_name)
+        context = {'phones': phone_objects}
+        return render(request, template, context)
+    except ValueError as e:
+        print(f'Ошибка: {e}')
+        return HttpResponse(e)
+    except Exception as ex:
+        print(f'Ошибка: {ex}')
         return HttpResponse("Ошибка при сортировке!")
-    context = {'phones': phone_objects}
-    return render(request, template, context)
 
 
 def show_product(request, slug):
