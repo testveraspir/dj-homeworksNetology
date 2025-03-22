be84e9e6580a85569fc4bb8bbed076e16e72cf62
Vera Spiridonova <ver-spiridonova@yandex.ru>
2025-03-22 19:48:00 +0300

    Added nullable

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -12,6 +12,7 @@ class Sensor(models.Model):
 class Measurement(models.Model):
     temperature = models.DecimalField(max_digits=3, decimal_places=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    nullable = models.ImageField(upload_to='images/', blank=True)
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
 
     def __str__(self):
--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -6,7 +6,7 @@ from measurement.models import Measurement, Sensor
 class MeasurementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Measurement
-        fields = ['temperature', 'created_at']
+        fields = ['temperature', 'created_at', 'nullable']
 
 
 class SensorListSerializer(serializers.ModelSerializer):
@@ -21,3 +21,10 @@ class SensorDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sensor
         fields = ['id', 'name', 'description', 'measurements']
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        for item in data['measurements']:
+            if 'nullable' in item and item['nullable'] is None:
+                item.pop('nullable')
+        return data
