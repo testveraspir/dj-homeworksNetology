32f5a7b5868c02b2a4902299793c1a6c9522292b
Vera Spiridonova <ver-spiridonova@yandex.ru>
2025-03-30 22:28:33 +0300

    Added model FavortieAdvertisement

--- 3.3-permissions/api_with_restrictions/advertisements/models.py
+++ 3.3-permissions/api_with_restrictions/advertisements/models.py
@@ -28,3 +28,15 @@ class Advertisement(models.Model):
     updated_at = models.DateTimeField(
         auto_now=True
     )
+
+
+class FavoriteAdvertisement(models.Model):
+    """Избранные объявления."""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    advertisement = models.ForeignKey(Advertisement,
+                                      on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("user", "advertisement")
