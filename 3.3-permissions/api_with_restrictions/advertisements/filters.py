f846aa7e60131ed35bb76fe288e73a08e1cb496f
Vera Spiridonova <ver-spiridonova@yandex.ru>
2025-03-30 23:15:39 +0300

    Added filter by FavoriteAdvertisement

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,12 +1,28 @@
 from django_filters import rest_framework as filters, DateFromToRangeFilter
+from django_filters.rest_framework import BooleanFilter
 
-from advertisements.models import Advertisement
+from advertisements.models import Advertisement, FavoriteAdvertisement
 
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
 
     created_at = DateFromToRangeFilter()
+    is_favorite = BooleanFilter(method='filter_is_favorite')
+
+    def filter_is_favorite(self, queryset, name, value):
+        user = self.request.user
+
+        if user.is_authenticated:
+            favorite_id = FavoriteAdvertisement.objects.filter(user=user). \
+                values_list("advertisement__id", flat=True)
+            # если is_favorite=True
+            if value:
+                return queryset.filter(id__in=favorite_id)
+            # если is_favorite=False
+            else:
+                return queryset.exclude(id__in=favorite_id)
+        return queryset
 
     class Meta:
         model = Advertisement
