ea60291eab8c370851f57a3c5cad87dab4bf0e52
Vera Spiridonova <ver-spiridonova@yandex.ru>
2025-03-30 22:19:01 +0300

    Added permission for admin

--- 3.3-permissions/api_with_restrictions/advertisements/permissions.py
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -5,4 +5,6 @@ class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method == "GET":
             return True
+        if request.user.is_staff:
+            return True
         return request.user == obj.creator
